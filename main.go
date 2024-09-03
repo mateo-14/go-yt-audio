79e43982c2c5b4707ee7bf9c7fd288bf2379c22c
Mateo Ledesma <mateo.14.ledesma@gmail.com>
2024-09-03 19:34:01 -0300

    Some changes

--- main.go
+++ main.go
@@ -1,16 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os"
 	"os/exec"
+	"strconv"
 	"sync"
 )
 
 var (
-	downloads = make(map[string]*sync.Cond)
+	downloads = sync.Map{}
 )
 
 func main() {
@@ -18,38 +20,40 @@ func main() {
 
 	mux.HandleFunc("GET /{url}", func(w http.ResponseWriter, r *http.Request) {
 		url := r.PathValue("url")
+
 		if url == "" || url == "favicon.ico" {
 			w.WriteHeader(http.StatusBadRequest)
 			w.Write([]byte("Invalid URL"))
 			return
 		}
 
-		if _, ok := downloads[url]; !ok {
+		var wg *sync.WaitGroup
+		if v, ok := downloads.Load(url); !ok {
 			if f, err := os.Open(url + ".opus"); err == nil {
 				log.Printf("File already downloaded %s. Streaming...", url)
 				streamFile(f, w)
 				return
 			}
 
-			downloads[url] = sync.NewCond(&sync.Mutex{})
+			wg = &sync.WaitGroup{}
+			wg.Add(1)
+			downloads.Store(url, wg)
+
 			go func() {
 				err := downloadYt(url)
 				if err != nil {
 					log.Printf("Error downloading %s: %s", url, err)
 				}
-				downloads[url].L.Lock()
-				defer delete(downloads, url)
-				defer downloads[url].L.Unlock()
-				downloads[url].Broadcast()
+				downloads.Delete(url)
+				wg.Done()
 			}()
 		} else {
+			wg = v.(*sync.WaitGroup)
 			log.Printf("Already downloading %s", url)
 		}
 
-		downloads[url].L.Lock()
-		defer downloads[url].L.Unlock()
-		log.Printf("Waiting for %s", url)
-		downloads[url].Wait()
+		log.Printf("Downloading %s", url)
+		wg.Wait()
 
 		if f, err := os.Open(url + ".opus"); err == nil {
 			log.Printf("File downloaded %s. Streaming...", url)
@@ -57,14 +61,13 @@ func main() {
 		} else {
 			http.Error(w, "Error reading file", http.StatusInternalServerError)
 		}
-
 	})
 
 	http.ListenAndServe(":8080", mux)
 }
 
 func downloadYt(id string) error {
-	cmd := exec.Command("yt-dlp", "-o", "-", "-x", id)
+	cmd := exec.Command("yt-dlp", "-o", "-", "-x", fmt.Sprintf("https://www.youtube.com/watch?v=%s", id))
 	stdout, err := cmd.StdoutPipe()
 	if err != nil {
 		return err
@@ -90,24 +93,8 @@ func downloadYt(id string) error {
 
 func streamFile(file *os.File, w http.ResponseWriter) {
 	w.Header().Set("Content-Type", "audio/ogg")
-	w.Header().Set("Transfer-Encoding", "chunked")
-
-	buff := make([]byte, 1024)
-	for {
-		n, err := file.Read(buff)
-		if err != nil && err != io.EOF {
-			http.Error(w, "Error reading file", http.StatusInternalServerError)
-			return
-		}
-
-		if n == 0 {
-			break
-		}
-		_, err = w.Write(buff[:n])
-		if err != nil {
-			return
-		}
-
-		w.(http.Flusher).Flush()
-	}
+	fsstat, _ := file.Stat()
+	filesize := fsstat.Size()
+	w.Header().Set("Content-Length", strconv.Itoa(int(filesize)))
+	io.Copy(w, file)
 }
