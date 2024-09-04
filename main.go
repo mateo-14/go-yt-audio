cea2aca3d808ef85bdd78ad7946a49aea6edd3a4
Mateo Ledesma <mateo.14.ledesma@gmail.com>
2024-09-03 23:14:25 -0300

    Add S3

--- main.go
+++ main.go
@@ -1,25 +1,65 @@
 package main
 
 import (
+	"bytes"
+	"cmp"
+	"context"
+	"database/sql"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os"
 	"os/exec"
-	"strconv"
 	"sync"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/subosito/gotenv"
 	"githubo.com/mateo-14/go-yt/ytdlp"
 )
 
 var (
-	downloads = sync.Map{}
+	downloads  = sync.Map{}
+	s3Client   *s3.Client
+	bucketName string
 )
 
 func main() {
 	log.SetOutput(os.Stdout)
+	err := gotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Get database connection
+	db, err := getDatabaseConnection()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	var publicBucket = "https://sn.mateoledesma.com"
+
+	bucketName = os.Getenv("S3_BUCKET_NAME")
+	if bucketName == "" {
+		log.Fatal("BUCKET_NAME is not set in .env file")
+	}
+
+	s3Client, err = getS3Client()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Create downloads table if not exists
+	if db.Exec("CREATE TABLE IF NOT EXISTS yt_downloads (id VARCHAR(255) PRIMARY KEY, downloaded_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, last_accessed TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP);"); err != nil {
+		log.Fatal(err)
+	}
+
 	ytdlp.CheckYtdl()
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("GET /{url}", func(w http.ResponseWriter, r *http.Request) {
 		url := r.PathValue("url")
@@ -30,23 +70,46 @@ func main() {
 			return
 		}
 
-		var wg *sync.WaitGroup
-		if v, ok := downloads.Load(url); !ok {
-			if f, err := os.Open(url + ".opus"); err == nil {
-				log.Printf("File already downloaded %s. Streaming...", url)
-				streamFile(f, w)
+		rows, err := db.Query("SELECT id FROM yt_downloads WHERE id = ?", url)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("Error querying database"))
+			log.Println(err)
+			return
+		}
+
+		if rows.Next() {
+			rows.Close()
+			_, err := db.Exec("UPDATE yt_downloads SET last_accessed = CURRENT_TIMESTAMP WHERE id = ?", url)
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				w.Write([]byte("Error updating database"))
+				log.Println(err)
 				return
 			}
 
+			log.Printf("File exists %s", url)
+			w.Header().Set("Location", publicBucket+"/"+url+".opus")
+			w.WriteHeader(http.StatusTemporaryRedirect)
+			return
+		}
+
+		var wg *sync.WaitGroup
+		if v, ok := downloads.Load(url); !ok {
 			wg = &sync.WaitGroup{}
 			wg.Add(1)
 			downloads.Store(url, wg)
 
 			go func() {
-				err := downloadYt(url)
-				if err != nil {
+				log.Printf("Downloading %s", url)
+				if err := downloadAndUploadAudio(r.Context(), url); err != nil {
 					log.Printf("Error downloading %s: %s", url, err)
+					w.WriteHeader(http.StatusInternalServerError)
+					w.Write([]byte("Error downloading audio"))
+					wg.Done()
+					return
 				}
+				db.Exec("INSERT INTO yt_downloads (id) VALUES (?)", url)
 				downloads.Delete(url)
 				wg.Done()
 			}()
@@ -55,22 +118,17 @@ func main() {
 			log.Printf("Already downloading %s", url)
 		}
 
-		log.Printf("Downloading %s", url)
 		wg.Wait()
 
-		if f, err := os.Open(url + ".opus"); err == nil {
-			log.Printf("File downloaded %s. Streaming...", url)
-			streamFile(f, w)
-		} else {
-			http.Error(w, "Error reading file", http.StatusInternalServerError)
-		}
+		w.Header().Set("Location", publicBucket+"/"+url+".opus")
+		w.WriteHeader(http.StatusTemporaryRedirect)
 	})
 
 	log.Println("Listening on :8080")
 	http.ListenAndServe(":8080", mux)
 }
 
-func downloadYt(id string) error {
+func downloadAndUploadAudio(ctx context.Context, id string) error {
 	cmd := exec.Command("./"+ytdlp.GetExecutableName(), "-f", "worst*[acodec=opus]", "-o", "-", fmt.Sprintf("https://www.youtube.com/watch?v=%s", id))
 	stdout, err := cmd.StdoutPipe()
 	if err != nil {
@@ -82,23 +140,77 @@ func downloadYt(id string) error {
 		return err
 	}
 
-	file, err := os.Create(id + ".opus")
+	content, err := io.ReadAll(stdout)
 	if err != nil {
 		return err
 	}
 
-	_, err = io.Copy(file, stdout)
-	if err != nil {
-		return err
-	}
+	_, err = s3Client.PutObject(ctx, &s3.PutObjectInput{
+		Bucket:      aws.String(bucketName),
+		Key:         aws.String(id + ".opus"),
+		Body:        bytes.NewReader(content),
+		ContentType: aws.String("audio/ogg"),
+	})
 
-	return nil
+	return err
 }
 
-func streamFile(file *os.File, w http.ResponseWriter) {
-	w.Header().Set("Content-Type", "audio/ogg")
-	fsstat, _ := file.Stat()
-	filesize := fsstat.Size()
-	w.Header().Set("Content-Length", strconv.Itoa(int(filesize)))
-	io.Copy(w, file)
+func getDatabaseConnection() (*sql.DB, error) {
+	dbHost := os.Getenv("DB_HOST")
+	if dbHost == "" {
+		return nil, fmt.Errorf("DB_HOST is not set in .env file")
+	}
+	dbUser := os.Getenv("DB_USER")
+	if dbUser == "" {
+		return nil, fmt.Errorf("DB_USER is not set in .env file")
+	}
+	dbDatabase := os.Getenv("DB_DATABASE")
+	if dbDatabase == "" {
+		return nil, fmt.Errorf("DB_DATABASE is not set in .env file")
+	}
+
+	dbPort := cmp.Or(os.Getenv("DB_PORT"), "3306")
+
+	dbPassword := os.Getenv("DB_PASSWORD")
+	if dbPassword != "" {
+		dbPassword = ":" + dbPassword
+	}
+
+	db, err := sql.Open("mysql", fmt.Sprintf("%s%s@tcp(%s:%s)/%s", dbUser, dbPassword, dbHost, dbPort, dbDatabase))
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func getS3Client() (*s3.Client, error) {
+	accountId := os.Getenv("R2_ACCOUNT_ID")
+	if accountId == "" {
+		return nil, fmt.Errorf("ACCOUNT_ID is not set in .env file")
+	}
+
+	accessKeyId := os.Getenv("S3_ACCESS_KEY_ID")
+	if accessKeyId == "" {
+		return nil, fmt.Errorf("S3_ACCESS_KEY_ID is not set in .env file")
+	}
+
+	accessKeySecret := os.Getenv("S3_ACCESS_KEY_SECRET")
+	if accessKeySecret == "" {
+		return nil, fmt.Errorf("S3_ACCESS_KEY_SECRET is not set in .env file")
+	}
+
+	cfg, err := config.LoadDefaultConfig(context.TODO(),
+		config.WithCredentialsProvider(credentials.NewStaticCredentialsProvider(accessKeyId, accessKeySecret, "")),
+		config.WithRegion("auto"),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	s3Client := s3.NewFromConfig(cfg, func(o *s3.Options) {
+		o.BaseEndpoint = aws.String(fmt.Sprintf("https://%s.r2.cloudflarestorage.com", accountId))
+	})
+
+	return s3Client, nil
 }
